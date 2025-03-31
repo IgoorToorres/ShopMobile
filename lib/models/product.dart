b2de4f1e0c7c20153fc8c08ed81b152b260d78a8
IgoorToorres <igorevangelista121@gmail.com>
2025-03-31 16:36:26 -0300

    favoritos salvando no banco

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/utils/constants.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,18 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  Future<void> toggleFavorite() async {
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final response = await http.patch(
+      Uri.parse('${Constants.PRODUCTS_BASEURL}/$id.json'),
+      body: jsonEncode({"isFavorite": isFavorite}),
+    );
+
+    if (response.statusCode >= 400) {
+      isFavorite = !isFavorite;
+      notifyListeners();
+    }
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  // ignore: constant_identifier_names
+  static const PRODUCTS_BASEURL = 'https://shop-app-b8858-default-rtdb.firebaseio.com/products';
+}
