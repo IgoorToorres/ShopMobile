0e8a86fcac9445a69e8ba4abd94589bde68b0ef1
IgoorToorres <igorevangelista121@gmail.com>
2025-04-10 18:05:59 -0300

    animacao tela do produto

--- lib/features/product_info/pages/product_detail_page.dart
+++ lib/features/product_info/pages/product_detail_page.dart
@@ -13,13 +13,15 @@ class ProductDetailPage extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Product;
     final cart = Provider.of<Cart>(context, listen: false);
     return Scaffold(
-      appBar: AppbarDefault(title: product.name),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.name),
+              background: Stack(
+                fit: StackFit.expand,
                 children: [
                   Hero(
                     tag: product.id,
@@ -30,78 +32,109 @@ class ProductDetailPage extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      'R\$ ${product.price.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0, 0.8),
+                        end: Alignment(0, 0),
+                        colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ],
                       ),
-                      textAlign: TextAlign.center,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      product.description,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.black87,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const SizedBox(height: 80), // Espaço para o botão fixo
                 ],
               ),
             ),
           ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 10,
-                  offset: Offset(0, -2),
-                )
-              ],
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-              cart.addItem(product);
-              ScaffoldMessenger.of(context).hideCurrentSnackBar();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: const Color.fromARGB(255, 34, 34, 34),
-                  content: Text('Produto adicionado com sucesso!'),
-                  duration: Duration(seconds: 2),
-                  action: SnackBarAction(
-                    label: 'DESFAZER',
-                    textColor: Colors.redAccent,
-                    onPressed: () {
-                      cart.removeSingleItem(product.id);
-                    },
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            product.description,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        const SizedBox(height: 80), // Espaço para o botão fixo
+                      ],
+                    ),
                   ),
                 ),
-              );
-            },
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 10,
+                        offset: Offset(0, -2),
+                      )
+                    ],
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      cart.addItem(product);
+                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor:
+                              const Color.fromARGB(255, 34, 34, 34),
+                          content: Text('Produto adicionado com sucesso!'),
+                          duration: Duration(seconds: 2),
+                          action: SnackBarAction(
+                            label: 'DESFAZER',
+                            textColor: Colors.redAccent,
+                            onPressed: () {
+                              cart.removeSingleItem(product.id);
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Comprar',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
                 ),
-              ),
-              child: const Text(
-                'Comprar',
-                style: TextStyle(fontSize: 18),
-              ),
+                SizedBox(
+                  height: 1000,
+                ),
+                Center(child: Text('fim')),
+              ],
             ),
           ),
         ],
