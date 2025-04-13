8b99c6ef49bf63082aa9ee5017bdea41cddd2712
IgoorToorres <igorevangelista121@gmail.com>
2025-04-12 23:38:04 -0300

    correcao

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
 class Constants {
-  // ignore: constant_identifier_names
   static const USER_ORDERS_URL = 'https://shop-app-b8858-default-rtdb.firebaseio.com/userOrders';
-  // ignore: constant_identifier_names
   static const USER_FAVORITES_URL = 'https://shop-app-b8858-default-rtdb.firebaseio.com/userFavorites';
-  // ignore: constant_identifier_names
   static const PRODUCT_BASEURL = 'https://shop-app-b8858-default-rtdb.firebaseio.com/products';
 }
