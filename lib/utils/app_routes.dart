af1cfb24746f85419582ebf946fd61d3d33c2cd1
IgoorToorres <igorevangelista121@gmail.com>
2025-04-12 23:35:00 -0300

    correcao

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,14 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
 class AppRoutes{
-  // ignore: constant_identifier_names
   static const AUTH_OR_HOME = '/';
-  // ignore: constant_identifier_names
   static const PRODUCT_DETAIL = '/product-detail';
-  // ignore: constant_identifier_names
   static const CART = '/cart';
-  // ignore: constant_identifier_names
   static const ORDERS = '/orders';
-  // ignore: constant_identifier_names
   static const PRODUCTS = '/products';
-  // ignore: constant_identifier_names
   static const PRODUCT_FORM = '/products-form';
 }
